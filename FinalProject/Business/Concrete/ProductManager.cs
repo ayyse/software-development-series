83525339a2eb3374f9a20a594c722aa363cc76ed
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-02-26 13:06:56 +0300

    Add files via upload

---
+++ FinalProject/Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        // bir business class başka classları newlemez
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
